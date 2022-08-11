01dbb471c1c160a8750b53efe83bbf6936ddf9f8
LTH <johtta888@naver.com>
2022-08-11 14:50:03 +0900

    Feat: 추가 BottomTab navigate

--- src/components/@modules/BottomTab/index.tsx
+++ src/components/@modules/BottomTab/index.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { SvgIcon } from "components/@base";
+import { activities, useFlow } from "stackflow";
 
 function BottomTab() {
+  const { replace } = useFlow();
+  const onNavigate = useCallback((screen: keyof typeof activities) => {
+    replace(screen, {});
+  }, []);
+
   return (
-    <div className="p-4 flex justify-center items-center">
-      <button type="button" className="flex-1 flex justify-center">
+    <div className="p-8 flex justify-center items-center fixed bottom-0 inset-x-0 border-t-2 border-solid">
+      <button
+        type="button"
+        onClick={() => onNavigate("HomeScreen")}
+        className="flex-1 flex justify-center"
+      >
         <SvgIcon name="homeSmile" />
       </button>
-      <button type="button" className="flex-1 flex justify-center">
+      <button
+        type="button"
+        onClick={() => onNavigate("TodoScreen")}
+        className="flex-1 flex justify-center"
+      >
         <SvgIcon name="check" />
       </button>
-      <button type="button" className="flex-1 flex justify-center">
+      <button
+        type="button"
+        onClick={() => onNavigate("SettingsScreen")}
+        className="flex-1 flex justify-center"
+      >
         <SvgIcon name="settings" />
       </button>
     </div>
