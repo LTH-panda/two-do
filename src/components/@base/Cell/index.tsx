5e229564e12c91c80a666b31b5b2359bfb3c9530
LTH <johtta888@naver.com>
2022-08-22 15:41:00 +0900

    Design: 추가 Cell 레이아웃

--- src/components/@base/Cell/index.tsx
+++ src/components/@base/Cell/index.tsx
@@ -7,8 +7,8 @@ type CellProps = {
 
 function Cell({ title, right }: CellProps) {
   return (
-    <div className="flex p-8 ">
-      <div className="text-xl font-primary">{title}</div>
+    <div className="flex items-center p-8">
+      <div className="mr-auto text-xl font-primary">{title}</div>
       <span>{right}</span>
     </div>
   );
